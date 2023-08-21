44fae199c39be16e2b7f5494e8e0e9eb14f85f9b
ragnarlodbrok1992 <maraus92@gmail.com>
2023-08-21 16:43:30 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use macroquad::prelude::*;
 #[macroquad::main("Rusteroids")] // Bar Title
 async fn main() {
     loop {
+        // Input and other events <-- here
+        if is_key_down(KeyCode::Escape) {
+            break;
+        }
+
+        // Simulate here
+
         // Main render loop
         clear_background(GRAY);
 
